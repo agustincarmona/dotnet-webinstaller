82c2fd7530c1d920438172f9f33f41ec8530cfad
Agustín Carmona <agustin.carmona@gmail.com>
2016-04-12 17:36:01 +0200

    upload project

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //if (ApplicationDeployment.IsNetworkDeployed)
+            //{
+            //    if (ApplicationDeployment.CurrentDeployment.CurrentVersion != ApplicationDeployment.CurrentDeployment.UpdatedVersion)
+            //    {
+            //        MessageBox.Show("Download last version from server");
+            //        Application.ExitThread();
+            //        Application.Restart();
+            //    }
+            //}
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
